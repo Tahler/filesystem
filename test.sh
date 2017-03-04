42db090d6de5c81b4631e9ce7c61aa93b9cd36c1
Tyler Berry <tyler@berryac.com>
2017-03-03 22:05:51 -0700

    Test script cleans up too

--- test.sh
+++ test.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-make -C mkfs run "ARGS=-b 1024 -s 131072 fs_file"
+make -C mkfs run "ARGS=-b 1024 -s 131072 fs_file" > /dev/null
 mv mkfs/fs_file .
 echo "------------";
-make -C rtfs
+make -C rtfs > /dev/null
 mv rtfs/target/fs .
 ./fs
+make -C mkfs clean > /dev/null
+make -C rtfs clean > /dev/null
+rm fs
+rm fs_file
